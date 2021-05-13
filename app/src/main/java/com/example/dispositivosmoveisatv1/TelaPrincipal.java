53caa6c94e6cae71dee7a987240770cb3ef828f5
Aline Silva <aline.silva@e-unicred.com.br>
2021-05-13 20:18:39 -0300

    app

---
+++ app/src/main/java/com/example/dispositivosmoveisatv1/TelaPrincipal.java
@@ -0,0 +1,37 @@
+package com.example.dispositivosmoveisatv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TelaPrincipal extends AppCompatActivity {
+    Button inicio;
+    Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+        inicio = findViewById(R.id.inicio);
+        login = findViewById(R.id.telaLogin);
+
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TelaPrincipal.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TelaPrincipal.this, Home.class);
+                startActivity(i);
+            }
+        });
+    }
+}
